4869ea7d1176d9e257a3d66cf4cd6a91deac3e52
bayu <bayudiarta230@gmail.com>
2022-07-27 12:28:18 +0700

    add status code authorization jwt

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -22,11 +22,11 @@ class JwtMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
+                return response()->json(['status' => 'Token is Invalid', 'code' => 401]);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
+                return response()->json(['status' => 'Token is Expired', 'code' => 401]);
             }else{
-                return response()->json(['status' => 'Authorization Token not found']);
+                return response()->json(['status' => 'Authorization Token not found', 'code' => 404]);
             }
         }
         return $next($request);
